23b79ef19aa8ea19a4eef3ae405781125b3d6416
AbsurdBartholomew <43071219+AbsurdBartholomew@users.noreply.github.com>
2025-03-31 15:22:38 -0400

    Add build information for other versions

---
+++ source/system/DEBUG/EUR/CD/info.cpp
@@ -0,0 +1,5 @@
+#include "source/system/info.h"
+
+char INF_Version[] = "DEBUG";
+char INF_Territory[] = "EUR";
+char INF_FileSystem[] = "CD";
---
+++ source/system/FINAL/EUR/PC/info.cpp
@@ -0,0 +1,5 @@
+#include "source/system/info.h"
+
+char INF_Version[] = "FINAL";
+char INF_Territory[] = "EUR";
+char INF_FileSystem[] = "PC";
